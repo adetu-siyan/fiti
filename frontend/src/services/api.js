44c9d21aa071dbcfdabfa61f294f307f568e01c0
adetu-siyan <adetumosgad@gmail.com>
2026-05-24 21:36:56 +0100

    fix mobile API connection by removing localhost fallback

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
+  baseURL: "https://fiti.onrender.com",
+  // baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
   timeout: 2000000
 })
 
